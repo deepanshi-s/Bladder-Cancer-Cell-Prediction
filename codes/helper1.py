706a20765a5b5ed0eabd20cad49a804e125dd354
Deepanshi <44083363+deepanshi-s@users.noreply.github.com>
2021-07-31 02:12:18 +0530

    Update helper1.py

--- codes/helper1.py
+++ codes/helper1.py
@@ -1,1 +1,124 @@
+#helper funtion for training and testing
+
+from keras.utils import to_categorical
+import numpy as np 
+import os
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras import backend as keras
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler,Callback
+import PIL
+import warnings
+import os
+import cv2
+from io import StringIO
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+'''
+def adjustData(img,mask):
+    label = mask[0]
+    label = np.mean(label,axis=2)/255
+    label = to_categorical(label,3)
+    label = np.reshape(label,(1,)+label.shape)
+    img = img/255
+    return (img,label) 
+'''
+
+
+
+def one_hot_it(image,label):
+    """
+    Convert a segmentation image label array to one-hot format
+    by replacing each pixel value with a vector of length num_classes
+
+    # Arguments
+        label: The 2D array segmentation image label
+        label_values
+        
+    # Returns
+        A 2D array with the same width and hieght as the input, but
+        with a depth size of num_classes
+    """
+    # st = time.time()
+    # w = label.shape[0]
+    # h = label.shape[1]
+    # num_classes = len(class_dict)
+    # x = np.zeros([w,h,num_classes])
+    # unique_labels = sortedlist((class_dict.values()))
+    # for i in range(0, w):
+    #     for j in range(0, h):
+    #         index = unique_labels.index(list(label[i][j][:]))
+    #         x[i,j,index]=1
+    # print("Time 1 = ", time.time() - st)
+
+    # st = time.time()
+    # https://stackoverflow.com/questions/46903885/map-rgb-semantic-maps-to-one-hot-encodings-and-vice-versa-in-tensorflow
+    # https://stackoverflow.com/questions/14859458/how-to-check-if-all-values-in-the-columns-of-a-numpy-matrix-are-the-same
+    image=image/255.
+    label_values=[(200,50,50),(255,255,255),(90,90,180)]
+    semantic_map = []
+    for colour in label_values:
+        # colour_map = np.full((label.shape[0], label.shape[1], label.shape[2]), colour, dtype=int)
+        equality = np.equal(label, colour)
+        class_map = np.all(equality, axis = -1)
+        semantic_map.append(class_map)
+    semantic_map = np.stack(semantic_map, axis=-1)
+    semantic_map/255.
+    # print("Time 2 = ", time.time() - st)
+
+    return (image,semantic_map)
+
+def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "rgb",
+                    mask_color_mode = "rgb",image_save_prefix  = "image",mask_save_prefix  = "mask",
+                    flag_multi_class = True,num_class = 3,save_to_dir = None,target_size = (512,512),seed = 1):
+    '''
+    can generate image and mask at the same time
+    use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
+    if you want to visualize the results of generator, set save_to_dir = "your path"
+    '''
+    image_datagen = ImageDataGenerator(**aug_dict)
+    mask_datagen = ImageDataGenerator(**aug_dict)
+    image_generator = image_datagen.flow_from_directory(
+        train_path,
+        classes = [image_folder],
+        class_mode = None,
+        color_mode = image_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = image_save_prefix,
+        seed = seed)
+    mask_generator = mask_datagen.flow_from_directory(
+        train_path,
+        classes = [mask_folder],
+        class_mode = None,
+        color_mode = mask_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = mask_save_prefix,
+        seed = seed)
+    train_generator = zip(image_generator, mask_generator)
+    for (img,mask) in train_generator:
+        img,mask =  one_hot_it(img,mask)
+        yield (img,mask)
+        
+
+
+
+def testGenerator(test_path,num_image = 5,target_size = (512,512),flag_multi_class = True,as_gray = False):
+    for i in range(num_image):
+        img = io.imread(os.path.join(test_path,"%d.png"%i),as_gray = as_gray)
+        img = img / 255
+        
+        img = trans.resize(img,target_size)
+        
+        img = np.reshape(img,(1,)+img.shape)
+
+        yield img
+        
 
